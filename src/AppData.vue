9f5189ef5b52fe4d4cd4d09b38e652e284bc0419
Anthon Holmqvist <anthon.kendel@gmail.com>
2020-01-25 18:15:16 +0100

    Add logo

---
+++ src/AppData.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="container">
+    <a
+      class="container-item"
+      v-for="(item, index) in appData"
+      :key="index"
+      :href="item.source"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <h2 class="title">
+        {{ item.title }}
+      </h2>
+      <p class="description">{{ item.description }}</p>
+    </a>
+  </div>
+</template>
+
+<script lang="ts">
+import { appData } from '@/AppData';
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: 'AppData',
+  data() {
+    return { appData };
+  }
+});
+</script>
---
+++ src/AppLogo.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="logo">
+    <img class="logo-item" src="./assets/cover.png" alt="OW Potions" />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+export default Vue.extend({
+  name: 'AppLogo'
+});
+</script>
